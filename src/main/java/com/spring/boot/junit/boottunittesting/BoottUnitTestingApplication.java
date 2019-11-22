367ff0240dbad22578dd4461cafb5fdc44c17520
uppalavijaygit <uppalavijay@gmail.com>
2019-11-22 21:13:17 +0000

    Initial

---
+++ src/main/java/com/spring/boot/junit/boottunittesting/BoottUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.junit.boottunittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoottUnitTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoottUnitTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/boot/junit/boottunittesting/BoottUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.junit.boottunittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoottUnitTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
